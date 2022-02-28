7acaaa9f398489b3cd50a1d6205ff9b60ec94693
Blueplanetearth <79352254+Blueplanetearth@users.noreply.github.com>
2022-02-28 20:02:16 +0530

    Third Commit

--- src/main/java/com/example/demo/JenkinSpringDemoApplication.java
+++ src/main/java/com/example/demo/JenkinSpringDemoApplication.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import java.util.logging.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,10 @@ public class JenkinSpringDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinSpringDemoApplication.class, args);
+		
+		 Logger logger =  Logger.getLogger(JenkinSpringDemoApplication.class.getName());
+		 
+		 logger.info("Hello Jenkins from SpringBoot again");
 		
 		System.out.println("Hello Jenkins from Springboot project deploy on Github");
 	}
